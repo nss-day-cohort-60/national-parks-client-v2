ec6d1c3a1f1e9670e6308cd508833304c08f1de1
Hazel Preza <hazel.d.preza@gmail.com>
2023-01-26 15:18:44 -0600

    added imports to park page

--- src/components/landing/ParkPage.js
+++ src/components/landing/ParkPage.js
@@ -1,5 +1,10 @@
+import { useParams } from "react-router-dom"
+import { useState, useEffect } from "react"
+
 export const ParkPage = () => {
     
+    const park_id = useParams
+    const [park, setPark] = useState()
 
     return<>
     <h2>
