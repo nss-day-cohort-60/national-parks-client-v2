50e2d00ace3011322ac783ef7bab20953fb76578
Hazel Preza <hazel.d.preza@gmail.com>
2023-01-27 13:35:11 -0600

    started working on adding wildlife

--- src/components/parks/ParkPage.js
+++ src/components/parks/ParkPage.js
@@ -3,40 +3,69 @@ import { useState, useEffect } from "react"
 import { PhotoCarousel } from "../landing/PhotoCarousel"
 import "./Parks.css"
 import { NavBar } from "../nav/NavBar"
+import { ParkWildlife } from "./ParkWildlife"
 
 export const ParkPage = () => {
-    
-    const {park_id} = useParams()
+
+    const { park_id } = useParams()
     const [park, setPark] = useState({})
     const [parkPhotos, setParkPhotos] = useState([])
+    const [wildlife, setWildlife] = useState([])
 
-    useEffect (
-        () =>{
+    useEffect(
+        () => {
             fetch(`http://localhost:8088/parks/${park_id}`)
                 .then(response => response.json())
-                .then((parkObject)=>{
+                .then((parkObject) => {
                     setPark(parkObject)
-            })
+                })
         },
         []
     )
 
-    useEffect (
-        () =>{
+    useEffect(
+        () => {
             fetch(`http://localhost:8088/photos?park_id=${park_id}`)
                 .then(response => response.json())
-                .then((parkPhotoArray)=>{
+                .then((parkPhotoArray) => {
                     setParkPhotos(parkPhotoArray)
-            })
+                })
         },
         []
     )
 
-    return<>
-    <NavBar/>
-    {console.log(parkPhotos)}
-    <h1>{park.name}</h1>
-    <p>{park.history}</p>
-    <PhotoCarousel resource = {parkPhotos}/>
+    useEffect(
+        () => {
+            fetch(`http://localhost:8088/wildlife?${park_id}`)
+                .then(response => response.json())
+                .then((wildlifeArray) => {
+                    setWildlife(wildlifeArray)
+                })
+        },
+        []
+    )
+
+    return <>
+        <NavBar />
+        <div className="park-page--container">
+            <PhotoCarousel resource={parkPhotos} />
+            <section id="park-page--info">
+            <h1>{park.name}</h1>
+            <p>{park.history}</p>
+            <h1>Location</h1>
+            <p>{park.city},{park.state}</p>
+            <p>{park.latitude},{park.longitude}</p>
+            <h1></h1>
+            </section>
+        </div>
+        <div>
+            {
+                wildlife.map(
+                    (animal) => {
+                        return <h1>{animal.name}</h1>
+                    }
+                )
+            }
+        </div>
     </>
 }
