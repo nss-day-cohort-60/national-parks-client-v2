e18ba9ea48b83e4eea830864cf62cbcda9e99f21
Maia Dutta <mermaia33@gmail.com>
2023-01-27 09:47:13 -0600

    changed home page to landing

--- src/components/NationalParks.js
+++ src/components/NationalParks.js
@@ -5,22 +5,27 @@ import { NavBar } from "./nav/NavBar"
 import { Login } from "./auth/Login"
 import { Register } from "./auth/Register"
 import { Blogs } from "./blog/Blogs"
+import { Landing } from "./landing/Landing"
 
 
 export const NationalParks = () => {
-	return <Routes>
-		<Route path="/login" element={<Login />} />
-		<Route path="/register" element={<Register />} />
+	return (
+    <Routes>
+      <Route path="/home" element={<Landing />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
 
-		<Route path="*" element={
-			<Authorized>
-				<>
-					<NavBar />
-					<ApplicationViews />
-				</>
-			</Authorized>
-
-		} />
-	</Routes>
+      <Route
+        path="*"
+        element={
+          <Authorized>
+            <>
+              <ApplicationViews />
+            </>
+          </Authorized>
+        }
+      />
+    </Routes>
+  );
 }
 
--- src/components/landing/Landing.js
+++ src/components/landing/Landing.js
@@ -1,8 +1,10 @@
 import { PhotoCarousel } from "./PhotoCarousel" 
 import { ParkList } from "../parks/ParksList"
+import { NavBar } from "../nav/NavBar"
 
 export const Landing = () => {
     return <>
+    <NavBar />
     <h1 className="title--main">Explore National Parks</h1>
     <PhotoCarousel />
     <ParkList />
