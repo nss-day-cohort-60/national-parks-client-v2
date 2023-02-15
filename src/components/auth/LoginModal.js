45784884e23c2131c48a492c5d7078bc5109c18b
JohnMDoll <johnmdoll3@gmail.com>
2023-02-15 15:24:47 -0600

    added fetchIt for blog post to provide server with user auth info. Other small changes to correct blog page bugs

--- src/components/auth/LoginModal.js
+++ src/components/auth/LoginModal.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import { Link, useNavigate } from "react-router-dom";
