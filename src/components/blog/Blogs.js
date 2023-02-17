c570af1dab3fb45bca14e024e4135fa5ba58e937
Maia Dutta <mermaia33@gmail.com>
2023-02-17 10:54:49 -0600

    added bootstrap cards for responsive design

--- src/components/blog/Blogs.js
+++ src/components/blog/Blogs.js
@@ -1,62 +1,77 @@
-import { useEffect, useState } from "react"
-import "./Blogs.css"
-import { AddBlog } from "./AddBlog"
-import { EditBlog } from "./EditBlog"
-import { FavoriteBtn } from "../favorites/favoriteBtn"
+import { useEffect, useState } from "react";
+import "./Blogs.css";
+import { AddBlog } from "./AddBlog";
+import { EditBlog } from "./EditBlog";
+import { FavoriteBtn } from "../favorites/favoriteBtn";
 
+const MyCard = ({ title, image, body }) => {
+  return (
+    <div className="card my-5 mx-5">
+      <div className="row no-gutters">
+        {image ? (
+          <div className="col-sm-12 col-md-4">
+            <img src={image} className="img-fluid" alt="blog-img" />
+          </div>
+        ) : (
+          ""
+        )}
+
+        <div className="col">
+          <div className="card-block px-2">
+            <h4 className="card-title">{title}</h4>
+            <p className="card-text">{body}</p>
+          </div>
+        </div>
+      </div>
+      <div>
+        <FavoriteBtn />
+      </div>
+    </div>
+  );
+};
 
 export const Blogs = ({ searchTermState, blogs }) => {
-    const [filteredBlogs, setFiltered] = useState(blogs)
-    const [blogModal, setBlogModal] = useState(false)
-    const [loggedIn, setLoggedIn] = useState(false)
+  const [filteredBlogs, setFiltered] = useState(blogs);
+  const [blogModal, setBlogModal] = useState(false);
+  const [loggedIn, setLoggedIn] = useState(false);
 
-    useEffect(
-        () => {
-            setFiltered(blogs)
-        },
-        [ searchTermState, blogs ]
-    )
+  useEffect(() => {
+    setFiltered(blogs);
+  }, [searchTermState, blogs]);
 
-    useEffect(() => {
-        const user = localStorage.getItem("np_token");
-        if (user) {
-            setLoggedIn(true)
-        }
-        else {
-            setLoggedIn(false)
-        }
-    }, [])
-
-    const toggleBlogModal = () => {
-        setBlogModal(!blogModal)
+  useEffect(() => {
+    const user = localStorage.getItem("np_token");
+    if (user) {
+      setLoggedIn(true);
+    } else {
+      setLoggedIn(false);
     }
-    
-        return (
-        <div>
-            <div className="blogs-panel">
-                <article className="blogs" >{loggedIn?
-                    <button className="save-blog" onClick={toggleBlogModal}>Add a Blog</button>:""}
-                    <div className="blogs-block">
-                        {blogs.map(
-                            (blog) => {
-                                return <section className="blog" key={`blog--${blog.id}`} id={`${blog.id}`}>
-                                    <header className="blogs">
-                                        <div className="blog-details">
-                                        <div className="title"><h3>{blog.title}</h3></div> 
-                                        <div className="secondRow">
-                                        {blog.photo?.url ? <img src={blog.photo?.url} alt="blog photos" className="blog-img"/> : <div className="no-image"></div>}
-                                        <div className="blog-post"><p>{blog.post_body}</p></div></div>
-                                        
-                                        </div><><FavoriteBtn /></>
-                                        </header>
-                                </section>
-                            }
-                        )
-                        }
-                    </div>
-                </article>
-                {blogModal && <AddBlog setBlogModal={setBlogModal} />}
-            </div>
-        </div>
-        )
-    }
+  }, []);
+
+  const toggleBlogModal = () => {
+    setBlogModal(!blogModal);
+  };
+
+  return (
+    <div>
+      {loggedIn ? (
+        <button className="save-blog" onClick={toggleBlogModal}>
+          Add a Blog
+        </button>
+      ) : (
+        ""
+      )}
+      <div>
+        {blogs.map((blog) => (
+          <MyCard
+            key={`blog--${blog.id}`}
+            title={blog.title}
+            image={blog.photo?.url}
+            body={blog.post_body}
+          />
+        ))}
+      </div>
+      {blogModal && <AddBlog setBlogModal={setBlogModal} />}
+    </div>
+  );
+};
--- src/components/parks/ParkPage.js
+++ src/components/parks/ParkPage.js
@@ -3,8 +3,53 @@ import { useState, useEffect } from "react";
 import { PhotoCarousel } from "../landing/PhotoCarousel";
 import "./parks.css";
 import { fetchIt } from "../auth/fetchIt";
-
 import { FavoriteBtn } from "../favorites/favoriteBtn";
+
+const COMING_SOON =
+  "https://images.pexels.com/photos/4439444/pexels-photo-4439444.jpeg";
+
+const PictureCard = ({ title, image, body, created }) => {
+  return (
+    <div className="card my-2">
+      <div className="row no-gutters">
+        {image ? (
+          <div className="col-sm-12 col-md-4">
+            <img src={image} className="img-fluid" alt="blog-img" />
+          </div>
+        ) : (
+          ""
+        )}
+        <div className="col">
+          <div className="card-block px-2">
+            <h4 className="card-title">{title}</h4>
+            <p className="card-text">{body}</p>
+          </div>
+        </div>
+      </div>
+      {created ? (
+        <div className="card-footer w-100 text-muted">Posted on {created}</div>
+      ) : (
+        ""
+      )}
+    </div>
+  );
+};
+
+const TextCard = ({ title, subtitle, body }) => {
+  return (
+    <div className="card col-sm-12 col-md-3 my-2 mx-2">
+      <div className="card-body">
+        <h5 className="card-title">{title}</h5>
+        {subtitle ? (
+          <h6 className="card-subtitle mb-2 text-muted">{subtitle}</h6>
+        ) : (
+          ""
+        )}
+        <p className="card-text">{body}</p>
+      </div>
+    </div>
+  );
+};
 
 export const ParkPage = () => {
   const { park_id } = useParams();
@@ -14,7 +59,8 @@ export const ParkPage = () => {
   const [campgrounds, setCampgrounds] = useState([]);
   const [amenities, setAmenities] = useState([]);
   const [naturalAttractions, setNaturalAttractions] = useState([]);
-  const [blogs, setBlogs] = useState([])
+  const [blogs, setBlogs] = useState([]);
+  const [parkImg, setParkImg] = useState(COMING_SOON);
 
   useEffect(() => {
     fetch(`http://localhost:8000/parks/${park_id}`)
@@ -29,6 +75,7 @@ export const ParkPage = () => {
       .then((response) => response.json())
       .then((parkPhotoArray) => {
         setParkPhotos(parkPhotoArray);
+        setParkImg(parkPhotoArray[0].url);
       });
   }, []);
 
@@ -76,18 +123,18 @@ export const ParkPage = () => {
     return amenities.map((amenity) => {
       if (amenity.name !== null) {
         return (
-          <>
+          <div key={amenity.id}>
             <p>
               {" "}
               - <b>{amenity.name}</b> || {amenity.amenity.type}
             </p>
-          </>
+          </div>
         );
       } else {
         return (
-          <>
+          <div key={amenity.id}>
             <p>- {amenity.amenity.type}</p>
-          </>
+          </div>
         );
       }
     });
@@ -95,71 +142,71 @@ export const ParkPage = () => {
 
   return (
     <>
-      <div className="park-page--container">
-        <PhotoCarousel resource={parkPhotos} />
-        <section id="park-page--info">
-          <h1>{park.name}</h1>
-          <p>{park.history}</p>
-          <h1>Location</h1>
-          <p>
-            {park.city},{park.state}
-          </p>
-          <p>
-            {park.latitude},{park.longitude}
-          </p>
-          <FavoriteBtn />
-        </section><h1 className="park-page--title"><b>Blogs about {park.name}</b></h1>
-        <div className="park--blogs" id="blog--container">
-          
-          {blogs.map((blog) => {
-            return (
-              <>
-                <h2>{blog.title}</h2>
-                <h6>{blog.date_created}</h6>
-                <div>
-                {blog?.photo !== null? <img src={blog?.photo?.url} className="park-page--sect-photo" />: ""}
-                <p className="park--blog" >{blog.post_body}</p>
-                </div>
-              </>
-            );
-          })}
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-12 col-md-6">
+            <img src={parkImg} alt="park-photo" className="img-fluid" />
+          </div>
+          <div className="col">
+            <section>
+              <h1>{park.name}</h1>
+              <p>{park.history}</p>
+              <h1>Location</h1>
+              <p>
+                {park.city},{park.state}
+              </p>
+              <p>
+                {park.latitude},{park.longitude}
+              </p>
+              <FavoriteBtn />
+            </section>
+          </div>
         </div>
-        <div className="park--blogs">
-          <h1 className="park-page--title"><b>Wildlife at {park.name}</b></h1>
-          {wildlife.map((animal) => {
-            return (
-              <>
-                <h2>{animal.name}</h2><div>
-                <img src={animal.image} className="park-page--sect-photo" />
-                <p className="park--blog">{animal.information}</p>
-                </div>
-              </>
-            );
-          })}</div><div>
-          <h1 className="park-page--title"><b>Campgrounds at {park.name}</b></h1>
-          {campgrounds.map((camp) => {
-            return (
-              <>
-                <h3>
-                  {camp.name} | Available sites: {camp.available_sites}
-                </h3>
-                <p>{camp.description}</p>
-              </>
-            );
-          })}
-          <h1 className="park-page--title"><b>Amenities at {park.name}</b></h1>
+
+        <div className="row">
+          <h1>Blogs</h1>
+          {blogs.map((blog) => (
+            <PictureCard
+              title={blog.title}
+              image={blog?.photo?.url}
+              body={blog.post_body}
+              created={blog.date_created}
+            />
+          ))}
+        </div>
+        <div className="row">
+          <h1>Wildlife</h1>
+          {wildlife.map((animal) => (
+            <PictureCard
+              title={animal.name}
+              image={animal.image}
+              body={animal.information}
+            />
+          ))}
+        </div>
+        <div className="row">
+          <h1>Campgrounds</h1>
+          {campgrounds.map((camp) => (
+            <TextCard
+              title={camp.name}
+              subtitle={`Available sites: ${camp.available_sites}`}
+              body={camp.description}
+            />
+          ))}
+        </div>
+        <div className="row">
+          <h1>Amenities</h1>
           {Amenity()}
-
-          <h1 className="park-page--title"><b>Natural Attractions at {park.name}</b></h1>
-
-          {naturalAttractions.map((attraction) => {
-            return (
-              <div key={`attraction-${attraction.id}`}>
-                <h3>{attraction.name}</h3>
-                <p>{attraction.description}</p>
-              </div>
-            );
-          })}
+        </div>
+        <div className="row">
+          <h1>Natural Attractions</h1>
+          {naturalAttractions.map((attraction) => (
+            <TextCard
+              key={`attraction-${attraction.id}`}
+              title={attraction.name}
+              body={attraction.description}
+            />
+          ))}
         </div>
       </div>
     </>
