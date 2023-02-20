52af3ce374e055f8bde6432a45669c4503846289
Maia Dutta <mermaia33@gmail.com>
2023-02-20 08:09:48 -0600

    small changes

--- src/components/favorites/Hub1.js
+++ src/components/favorites/Hub1.js
@@ -69,7 +69,9 @@ export const UserHub1 = () => {
     <>
       <div className="container-fluid">
         <div className="row">
-          <h1 className="d-flex justify-content-center hub-title">Your Favorites Hub</h1>
+          <h1 className="d-flex justify-content-center hub-title">
+            Your Favorites Hub
+          </h1>
         </div>
         <div className="row mt-2 mx-5">
           <div className="col-sm-12 col-md-12 mt-5 hub-carousel">
@@ -79,18 +81,18 @@ export const UserHub1 = () => {
         </div>
       </div>
       <div className="container-fluid">
-        <div className="row mx-5">
+        <div className="row mx-5 d-flex justify-content-around mb-5">
           <div className="col-sm-12 col-md-4">
             <h3 className="hub-subtitle">Events</h3>
-            <div className="box">{findFavorites(events)}</div>
+            <div className="box hub-events">{findFavorites(events)}</div>
           </div>
           <div className="col-sm-12 col-md-4">
             <h3 className="hub-subtitle">Parks</h3>
-            <div className="box">{findFavorites(parks)}</div>
+            <div className="box hub-parks">{findFavorites(parks)}</div>
           </div>
           <div className="col-sm-12 col-md-4">
             <h3 className="hub-subtitle">Blogs</h3>
-            <div className="box">{findFavorites(blogs)}</div>
+            <div className="box hub-blogs">{findFavorites(blogs)}</div>
           </div>
         </div>
       </div>
